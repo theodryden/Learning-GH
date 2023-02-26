26f7abae751fe9a4a24894c1e035f9b775111b40
theodryden <theodryden@yahoo.com>
2023-02-26 14:31:26 +0000

    Update ContentView to What's up!

--- Learning GH/ContentView.swift
+++ Learning GH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Code Squad!")
+            Text("What's up, Code Crew!")
         }
         .padding()
     }
