fadb66aaf71c90c140b35f5221580991bf90a884
TheoDryden <Theodryden@yahoo.com>
2023-02-26 13:49:02 +0000

    Initial Commit

---
+++ Learning GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning GH
+//
+//  Created by Theo Dryden on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
